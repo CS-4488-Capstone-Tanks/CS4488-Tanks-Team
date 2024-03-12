0c4dbdf427e5e69f46c345694d04aa39a2dc57b6
Luna Steed <lunasteed@isu.edu>
2024-03-11 18:43:28 -0600

    Replace Empty Scene/Renderer

--- Scene.h
+++ Scene.h
@@ -1,8 +1,50 @@
 #ifndef SCENE_H
 #define SCENE_H
 
-class Scene{
+#include <QObject>
 
+#include "gamestate.h"
+
+class Scene : public QObject
+{
+    Q_OBJECT
+
+public:
+    /**
+     * @brief Construct a new Scene object
+     * 
+     * @param float deltaTime
+     * @param char stateFilename[]
+     * @param QObject* parent = nullptr
+    */
+    explicit Scene(float deltaTime, char stateFilename[], QObject *parent = nullptr);
+
+    /**
+     * @brief Start the scene
+    */
+    void start();
+
+    // void keyPress();
+
+    /**
+     * @brief Pause or resume the scene
+     * 
+     * @param bool isPaused
+    */
+    void setPaused(bool isPaused);
+private:
+    // NetworkManager networkManager
+    GameState gameState;
+    float deltaTime; // This should probably be initialized in game.h, where the QTimer is
+    bool isPaused;
+
+signals:
+public slots:
+
+    /**
+     * @brief Update the scene
+    */
+    void update();
 };
 
 #endif // SCENE_H
