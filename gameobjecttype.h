8113590c9b83cb80163a762751df36168c58cd89
Dakoda Koziol <dakozioliz@gmail.com>
2024-03-29 19:08:24 -0600

    Can now remove gameobjects from gamestate given an entityID. removeObject() is now public. Added method to get string from GameObjectType (for debugging, mostly)

--- gameobjecttype.h
+++ gameobjecttype.h
@@ -1,6 +1,8 @@
 #ifndef GAMEOBJECTTYPE_H
 #define GAMEOBJECTTYPE_H
 
+#include <string>
+
 enum class GameObjectType
 {
     PlayerTank,
@@ -8,5 +10,22 @@ enum class GameObjectType
     Obstacle,
     Projectile
 };
+
+inline std::string gameObjectTypeToString(GameObjectType type)
+{
+    switch (type)
+    {
+        case GameObjectType::PlayerTank:
+            return "PlayerTank";
+        case GameObjectType::EnemyTank:
+            return "EnemyTank";
+        case GameObjectType::Obstacle:
+            return "Obstacle";
+        case GameObjectType::Projectile:
+            return "Projectile";
+        default:
+            return "Unknown type";
+    }
+}
 
 #endif // GAMEOBJECTTYPE_H
